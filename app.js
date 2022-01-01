f3a53714ae8ad7bb1a9b64a1ab21180eddd96d94
Sreeram Shenai <sreeramshenai2000@gmail.com>
2022-01-01 20:37:15 +0530

    Utility Bills Successfully added

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+var str = ""
+
 document.querySelector('.form-1').style.display='none';
 document.querySelector(".nav-utility").addEventListener('click',function () {
-    document.querySelector('.form-1').style.display='contents';
+    document.querySelector('.form-1').style.display='block';
+    document.querySelector('.btn-utility').addEventListener('click',function () {
+        str = document.querySelector('.in-utility').value;
+        document.querySelector('.form-1').style.display='none';
+        const txt = document.createTextNode(str);
+        const tag = document.createElement('div');
+        tag.appendChild(txt);
+        document.querySelector('.util-bills').appendChild(tag)
+        document.querySelector('.in-utility').value = ''
+    })
 })
 

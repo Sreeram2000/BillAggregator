12bc86367760950e7b3d3f3445eaa56372ada2d9
Sreeram Shenai <sreeramshenai2000@gmail.com>
2022-01-01 19:58:00 +0530

    add form-1

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 document.querySelector('.form-1').style.display='none';
+document.querySelector(".nav-utility").addEventListener('click',function () {
+    document.querySelector('.form-1').style.display='contents';
+})
+

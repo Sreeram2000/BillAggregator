fcfc812934e985de68b5f1af96212d638e373795
Sreeram Shenai <sreeramshenai2000@gmail.com>
2022-01-07 16:13:04 +0530

    box individual bills

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.querySelector(".nav-utility").addEventListener('click',function () {
         document.querySelector('.form-1').style.display='none';
         const txt = document.createTextNode(str);
         const tag = document.createElement('div');
+        tag.classList.add('bill-item')
         tag.appendChild(txt);
         document.querySelector('.util-bills').appendChild(tag)
         document.querySelector('.in-utility').value = ''
